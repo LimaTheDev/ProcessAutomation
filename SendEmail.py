2c129fd14b224c7123fc047f2e11fbad007885ed
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-08-11 20:57:21 -0300

    Create SendEmail.py
    Automatizado o envio de um e-mail com os dados de finanças máxima, mínima e atual da empresta escolhida.

---
+++ SendEmail.py
@@ -0,0 +1,56 @@
+import pyautogui
+import pyperclip
+import time
+import webbrowser
+from ProcessAutomation import ticker
+from DataAnalysis import maxima
+from DataAnalysis import minima
+from DataAnalysis import atual
+
+
+#position = pyautogui.position()
+#print(position)
+
+url = "https://www.google.com.br"
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+
+webbrowser.get(chrome_path).open(url)
+
+pyautogui.PAUSE = 2
+
+pyautogui.hotkey("ctrl", "t")
+
+pyperclip.copy("www.gmail.com")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("enter")
+
+time.sleep(10)
+
+pyautogui.click(x=56, y=205)
+
+pyperclip.copy("gestor@email.com")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+
+pyperclip.copy("Analises Diarias")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+
+mensagem = f"""
+Prezado Gestor,
+
+Seguem as análises,conforme solicitado, da ação {ticker} referente aos últimos 6 meses:
+
+Cotação máxima: R${maxima}
+Cotação mínima: R${minima}
+Cotação atual: R${atual}
+
+Qualquer dúvida, estou a disposição:
+
+atte
+"""
+pyperclip.copy(mensagem)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.click(x=1288, y=1008)
+
+
