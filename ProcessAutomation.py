168aa5370f493508238781b8080414f137230bff
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-08-11 20:53:31 -0300

    Update ProcessAutomation.py
    atualizado para com que o usuário consiga utilizar qualquer ação disponível no mercado

--- ProcessAutomation.py
+++ ProcessAutomation.py
@@ -1,6 +1,7 @@
 import yfinance as yf 
 
-dados = yf.Ticker("PETR4.SA").history("6mo")
+ticker = input("Digite o código da ação: ")
+dados = yf.Ticker(ticker).history("6mo")
 print(dados)
 
 fechamento = dados.Close
